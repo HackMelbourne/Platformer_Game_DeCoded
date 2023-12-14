9d7b6c6b6893ecdf6939ef77dac91426f91133e4
ayemyatmin <129644653+ayemyatmin@users.noreply.github.com>
2023-12-14 15:13:01 +1100

    vertical movement and collisions managed

---
+++ code/settings.py
@@ -0,0 +1,29 @@
+# level_map = [
+# '                            ',
+# '                            ',
+# '                            ',
+# ' XX    XXX            XX    ',
+# ' XX P                       ',
+# ' XXXX         XX         XX ',
+# ' XXXX       XX              ',
+# ' XX    X  XXXX    XX  XX    ',
+# '       X  XXXX    XX  XXX   ',
+# '    XXXX  XXXXXX  XX  XXXX  ',
+# 'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+'       XXX        XXX XX    ',
+'    P           X          ',
+'              XX       XXXX ',
+'            XX  XX         X',
+'          XXXX      XXX    X',
+'          XXXX        XX   X',
+'    XXXX  XXXXXX  XX  XXXXXX',
+'XXXXXXXXXXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
