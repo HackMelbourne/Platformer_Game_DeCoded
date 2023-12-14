a3569403b1b41027a34c24a724c9b2c6e2ae7065
ayemyatmin <129644653+ayemyatmin@users.noreply.github.com>
2023-12-14 14:18:09 +1100

    Creating player and giving it movement left and right

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 from tiles import Tile
 from settings import tile_size
+from player import Player
 
 class Level:
     def __init__(self, level_data, surface):
@@ -11,14 +12,25 @@ class Level:
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
                 if cell == 'X':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
 
     def run(self):
+        # Level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        # Player
+        self.player.update()
+        self.player.draw(self.display_surface)
---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32, 64))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(topleft = pos)
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 8
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.direction.x = 1
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_a]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * self.speed
