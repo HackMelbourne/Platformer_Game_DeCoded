685bbe091939e97e0a064a504f2bf9b408712a4d
ayemyatmin <129644653+ayemyatmin@users.noreply.github.com>
2023-12-14 13:42:05 +1100

    setup_level enumerate for each cell

--- level.py
+++ level.py
@@ -4,7 +4,13 @@ from tiles import Tile
 class Level:
     def __init__(self, level_data, surface):
         self.display_surface = surface
-        self.level_data = level_data
+        self.setup_level(level_data)
+
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                print(f'{row_index}, {col_index}:{cell}')
 
     def run(self):
-        pass
+        self.tiles.draw(self.display_surface)
