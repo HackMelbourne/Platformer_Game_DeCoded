06b7a1c20fcc114035295fad7766dbb38fe50c6d
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2023-12-29 01:11:53 +1100

    Add player on_ground state and implement multiple jumps
    - minor fix on left movement key

--- code/player.py
+++ code/player.py
@@ -11,28 +11,46 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
         self.gravity = 0.8
-        self.jump_speed = -16
+        self.jump_speed = -14
+
+        # Jumping and ground state
+        self.jump_count = 0
+        self.on_ground = False
+        self.max_jumps = 2
+
+        self.jumping = False
+        self.jump_key_pressed = False  # Track if jump key is still pressed
 
     def get_input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.direction.x = 1
-        elif keys[pygame.K_RIGHT] or keys[pygame.K_a]:
+        elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.direction.x = -1
         else:
             self.direction.x = 0
 
         if keys[pygame.K_UP] or keys[pygame.K_w] or keys[pygame.K_SPACE]:
-            self.jump()
+            if not self.jump_key_pressed:
+                self.jump()
+                self.jump_key_pressed = True
+        else:
+            self.jump_key_pressed = False
 
 
     def apply_gravity(self):
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
+        if self.on_ground:
+            self.jump_count = 0
 
     def jump(self):
-        self.direction.y = self.jump_speed
+        if self.jump_count < self.max_jumps:
+            self.direction.y = self.jump_speed
+            self.on_ground = False
+            self.jump_count += 1
+
 
     def update(self):
         self.get_input()
