90b0442932dc8094201472e535fbfdfd20202233
DoobieD00 <134687661+DoobieD00@users.noreply.github.com>
2024-02-19 14:19:50 +1100

    Update Door.py

--- code/Door.py
+++ code/Door.py
@@ -6,10 +6,10 @@ class Door(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         sprite = pygame.image.load(path.join(base_path, 'Tiles\window.png'))
-        sprite.set_colorkey((0, 0, 0))
-        sprite.convert_alpha()
-        sprite_surface = pygame.transform.smoothscale(sprite, (32, 64))
+        sprite_surface = pygame.transform.smoothscale(sprite, (32, 64)).convert_alpha()
+        sprite_surface.set_colorkey((0, 0, 0))
         self.image = sprite_surface
+        
         self.image.blit(sprite_surface, (0, 0))
         self.rect = self.image.get_rect(topleft=pos)
 
