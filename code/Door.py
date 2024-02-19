bf1e8fd741d25bac6564ed51282ac9ca433e0220
DoobieD00 <134687661+DoobieD00@users.noreply.github.com>
2024-02-19 12:17:05 +1100

    Update Door.py

--- code/Door.py
+++ code/Door.py
@@ -1,10 +1,11 @@
 import pygame
+from settings import base_path
 
 class Door(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.image = pygame.Surface((32, 64))
-        sprite = pygame.image.load('C:\\Users\\edber\\OneDrive\\Documents\\GitHub\\Retrying_Mario_platformer\\graphics\\Tiles\\window.png')
+        sprite = pygame.image.load(base_path, 'Tiles', 'window.png')
         sprite_surface = pygame.transform.smoothscale(sprite, (32, 64)).convert_alpha()
         sprite_surface.set_colorkey((0, 0, 0))
         self.image.blit(sprite_surface, (0, 0))
