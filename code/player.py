04706d9a6ddc986d3369b1b10e1f79d15265c786
lamooiety <clairesss.no1@gmail.com>
2024-03-10 23:44:23 +1100

    Player Animations and Attacks (refer to wiki 2.2)

--- code/player.py
+++ code/player.py
@@ -5,22 +5,22 @@ from settings import base_path
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        # self.CHARACTER_WIDTH = 50
-        # self.CHARACTER_HEIGHT = 60
-        # self.import_character_assets((self.CHARACTER_WIDTH, self.CHARACTER_HEIGHT))
+        self.CHARACTER_WIDTH = 50
+        self.CHARACTER_HEIGHT = 60
+        self.import_character_assets((self.CHARACTER_WIDTH, self.CHARACTER_HEIGHT))
 
         # Player animation state
-        # self.state = 'idle'
-        # self.facing_right = True
-        # self.frame_index = 0
-        # self.animation_speed = 0.30
-        # self.image = self.animations['idle'][self.frame_index]
-        # self.rect = self.image.get_rect(topleft=pos)
+        self.state = 'idle'
+        self.facing_right = True
+        self.frame_index = 0
+        self.animation_speed = 0.30
+        self.image = self.animations['idle'][self.frame_index]
+        self.rect = self.image.get_rect(topleft=pos)
 
         # Player appearance
-        self.image = pygame.Surface((32, 64))
-        self.image.fill('red')
-        self.rect = self.image.get_rect(topleft = pos)
+        # self.image = pygame.Surface((32, 64))
+        # self.image.fill('red')
+        # self.rect = self.image.get_rect(topleft = pos)
 
         # # Player attributes
         # self.max_health = 6
@@ -40,7 +40,7 @@ class Player(pygame.sprite.Sprite):
         self.jumping = False
         self.jump_key_pressed = False  # Track if jump key is still pressed
 
-        # self.is_attacking = False
+        self.is_attacking = False
         # self.is_invincible = False
         # self.is_invincible_timer = 0
         # self.attacked_time = 0
@@ -48,22 +48,22 @@ class Player(pygame.sprite.Sprite):
         # self.attacked_c = 0
 
 
-    # def import_character_assets(self,size:(int,int)):
-    #     character_path = path.join(base_path, 'freeknight')
-    #     self.animations = {'idle': [], 'run': [], 'jump': [], 'attack': [], 'dead': [], 'jumpAttack': [], 'walk': []}
-    #     self.health_icons = {
-    #         'full': pygame.transform.smoothscale(pygame.image.load(path.join(base_path, 'Tiles', 'dirt.png')), (size[0]//2, size[1]//2)),
-    #         'half': pygame.transform.smoothscale(pygame.image.load(path.join(base_path, 'Tiles', 'dirtHalf.png')), (size[0]//2, size[1]//2)),
-    #         'noHealth': pygame.transform.smoothscale(pygame.image.load(path.join(base_path, 'Tiles', 'boxAlt.png')), (size[0]//2, size[1]//2))
-    #     }
+    def import_character_assets(self,size:(int,int)):
+        character_path = path.join(base_path, 'freeknight')
+        self.animations = {'idle': [], 'run': [], 'jump': [], 'attack': [], 'dead': [], 'jumpAttack': [], 'walk': []}
+        self.health_icons = {
+            'full': pygame.transform.smoothscale(pygame.image.load(path.join(base_path, 'Tiles', 'dirt.png')), (size[0]//2, size[1]//2)),
+            'half': pygame.transform.smoothscale(pygame.image.load(path.join(base_path, 'Tiles', 'dirtHalf.png')), (size[0]//2, size[1]//2)),
+            'noHealth': pygame.transform.smoothscale(pygame.image.load(path.join(base_path, 'Tiles', 'boxAlt.png')), (size[0]//2, size[1]//2))
+        }
 
-    #     for animation in self.animations.keys():
-    #         full_path = path.join(character_path, animation.capitalize())
-    #         self.animations[animation] = [
-    #             pygame.transform.smoothscale(
-    #                 pygame.image.load(f'{full_path} ({i}).png').convert_alpha(),
-    #                 size)
-    #             for i in range(1, 11)]
+        for animation in self.animations.keys():
+            full_path = path.join(character_path, animation.capitalize())
+            self.animations[animation] = [
+                pygame.transform.smoothscale(
+                    pygame.image.load(f'{full_path} ({i}).png').convert_alpha(),
+                    size)
+                for i in range(1, 11)]
 
     # def deplete_health(self, amount=1):
     #     self.current_health -= amount
@@ -82,54 +82,54 @@ class Player(pygame.sprite.Sprite):
     #         else:
     #             surface.blit(self.health_icons['noHealth'], (x, dist_y))
 
-    # def animate(self):
-    #     animation = self.animations[self.state]
-    #     self.frame_index += self.animation_speed
+    def animate(self):
+        animation = self.animations[self.state]
+        self.frame_index += self.animation_speed
 
-    #     if self.frame_index >= len(animation):
-    #         if self.state == 'dead':
-    #             self.frame_index = len(animation) - 1
-    #         elif self.state == 'attack' or self.state == 'jumpAttack':
-    #             self.is_attacking = False
-    #             self.state = 'idle'
-    #         else:
-    #             self.frame_index = 0
+        if self.frame_index >= len(animation):
+            if self.state == 'dead':
+                self.frame_index = len(animation) - 1
+            elif self.state == 'attack' or self.state == 'jumpAttack':
+                self.is_attacking = False
+                self.state = 'idle'
+            else:
+                self.frame_index = 0
 
-    #     # Flip the image based on the direction
-    #     image = animation[int(self.frame_index)%len(animation)]
-    #     if self.is_invincible:
-    #         if (int(self.frame_index)%len(animation)) % 2 == 0:
-    #             image = pygame.mask.from_surface(image).to_surface()
-    #             image.set_colorkey((0,0,0))
-    #             image.set_alpha(20)
-    #     if self.facing_right:
-    #         self.image = image
-    #     else:
-    #         self.image = pygame.transform.flip(image, True, False)
+        # Flip the image based on the direction
+        image = animation[int(self.frame_index)%len(animation)]
+        # if self.is_invincible:
+        #     if (int(self.frame_index)%len(animation)) % 2 == 0:
+        #         image = pygame.mask.from_surface(image).to_surface()
+        #         image.set_colorkey((0,0,0))
+        #         image.set_alpha(20)
+        if self.facing_right:
+            self.image = image
+        else:
+            self.image = pygame.transform.flip(image, True, False)
 
-    # def update_state(self):
-    #     if self.attacked_c and self.on_ground:
-    #         self.attacked_c = 0
-    #         # checking if the player has already been knocked back then no need to knock back further.
-    #     if self.is_attacking:
-    #         return
-    #     elif self.direction.x > 0:
-    #         self.facing_right = True
-    #         if self.on_ground:
-    #             self.state = 'run'
-    #         else:
-    #             self.state = 'jump'
-    #     elif self.direction.x < 0:
-    #         self.facing_right = False
-    #         if self.on_ground:
-    #             self.state = 'run'
-    #         else:
-    #             self.state = 'jump'
-    #     else:
-    #         if self.on_ground:
-    #             self.state = 'idle'
-    #         else:
-    #             self.state = 'jump'
+    def update_state(self):
+        # if self.attacked_c and self.on_ground:
+        #     self.attacked_c = 0
+        #     # checking if the player has already been knocked back then no need to knock back further.
+        if self.is_attacking:
+            return
+        elif self.direction.x > 0:
+            self.facing_right = True
+            if self.on_ground:
+                self.state = 'run'
+            else:
+                self.state = 'jump'
+        elif self.direction.x < 0:
+            self.facing_right = False
+            if self.on_ground:
+                self.state = 'run'
+            else:
+                self.state = 'jump'
+        else:
+            if self.on_ground:
+                self.state = 'idle'
+            else:
+                self.state = 'jump'
 
     #     self.is_invincible_timer = pygame.time.get_ticks()
     #     if self.is_invincible and abs(self.attacked_time - self.is_invincible_timer) > 3000:
@@ -143,7 +143,7 @@ class Player(pygame.sprite.Sprite):
     #     if self.is_invincible and not self.on_ground:
     #         if abs(self.attacked_time - self.is_invincible_timer) > 3000:
     #             self.is_invincible = False
-    #             self.attacked_c = 0
+    #             # self.attacked_c = 0
     #             self.attacked_dir = 0
     #     else:
     #         self.attacked_time = attacked_time
@@ -151,14 +151,14 @@ class Player(pygame.sprite.Sprite):
     #         self.attacked_dir = dir
     #         self.attacked_c = 1
     #         self.current_health -= 1
-    #         #self.jump()
+            #self.jump()
 
     def get_input(self):
-        # if self.is_attacking:
-        #     self.direction.x = 0
-        #     return
+        if self.is_attacking:
+            self.direction.x = 0
+            return
         keys = pygame.key.get_pressed()
-        # mouse_buttons = pygame.mouse.get_pressed()
+        mouse_buttons = pygame.mouse.get_pressed()
 
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.direction.x = 1
@@ -178,18 +178,18 @@ class Player(pygame.sprite.Sprite):
         else:
             self.jump_key_pressed = False
 
-        # # Check for attack input (left mouse click or 'F' key)
-        # if mouse_buttons[0] or keys[pygame.K_f]:
-        #     self.attack()
+        # Check for attack input (left mouse click or 'F' key)
+        if mouse_buttons[0] or keys[pygame.K_f]:
+            self.attack()
 
-    # def attack(self):
-    #     if not self.is_attacking:
-    #         self.is_attacking = True
-    #         if self.on_ground:
-    #             self.state = 'attack'
-    #         else:
-    #             self.state = 'jumpAttack'
-    #         self.frame_index = 0
+    def attack(self):
+        if not self.is_attacking:
+            self.is_attacking = True
+            if self.on_ground:
+                self.state = 'attack'
+            else:
+                self.state = 'jumpAttack'
+            self.frame_index = 0
 
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -208,5 +208,5 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input()
-    #     self.update_state()
-    #     self.animate()
+        self.update_state()
+        self.animate()
