2676e795a31b2354a0a79a44c523e805019373d5
Abhijeet Kumar <abhijeetkumar12346@gmail.com>
2024-03-13 22:53:02 +1100

    update main

--- code/main.py
+++ code/main.py
@@ -8,14 +8,14 @@ from os import path
 class Main:
 
 # Pygame setup
-	def __init__(self) -> None:
+	def __init__(self):
 		pygame.init()
-		self.screen = pygame.display.set_mode((screen_width,screen_height))
+		self.screen = pygame.display.set_mode((screen_width, screen_height))
 		self.clock = pygame.time.Clock()
 		self.level = Level(level_map, self.screen)
 		self.main_menu = True
-		self.start_img = pygame.image.load(base_path + '\\buttons\\start-1.png').convert_alpha()
-		self.exit_img= pygame.image.load(base_path + '\\buttons\\exit-1.png').convert_alpha()
+		self.start_img = pygame.image.load(path.join(base_path, 'buttons', 'start-1.png')).convert_alpha()
+		self.exit_img = pygame.image.load(path.join(base_path, 'buttons', 'exit-1.png')).convert_alpha()
 		self.menu_img = pygame.image.load(path.join(base_path, 'title.png')).convert_alpha()
 
 		self.start_button = Button(screen_width//2 - self.start_img.get_width()//2, 500, self.start_img)
@@ -30,26 +30,8 @@ class Main:
 					pygame.quit()
 					sys.exit()
 
-	background = pygame.image.load(path.join(base_path, 'bg.png')).convert()
-	background = pygame.transform.smoothscale(background, screen.get_size())
-	screen.blit(background, (0, 0))
+			self.screen.fill((0, 0, 0))  # Clear the screen with black before drawing anything
 
-	if start_button.clicked:
-		main_menu = False
-
-	if exit_button.clicked:
-		pygame.quit()
-		sys.exit()
-
-	if main_menu == True:
-		start_button.draw(screen)
-		exit_button.draw(screen)
-	else:
-		level.run()
-
-
-	pygame.display.update()
-	clock.tick(clock_tick)
 			# Check if the game is in the main menu state
 			if self.main_menu:
 				# Load the main menu background image
@@ -59,7 +41,7 @@ class Main:
 				background_img = pygame.image.load(path.join(base_path, 'bg.png')).convert()
 
 			# Resize the background image to match the screen size
-			background_img = pygame.transform.smoothscale(background_img, self.screen.get_size())
+			background_img = pygame.transform.scale(background_img, self.screen.get_size())
 
 			# Blit the background image onto the screen
 			self.screen.blit(background_img, (0, 0))
@@ -70,17 +52,19 @@ class Main:
 			if not self.level.check_player():
 				return 1
 
-			if self.main_menu == True:
+			if self.main_menu:
 				# Draw the main menu buttons
 				self.screen.blit(self.menu_img, (screen_width//2 - self.menu_img.get_width()//2, 50))
 				self.start_button.draw(self.screen)
 				self.exit_button.draw(self.screen)
 
-			if self.start_button.clicked:
-				self.main_menu = False
+				if self.start_button.clicked:
+					self.main_menu = False
 
-			if self.exit_button.clicked:
-				return 2
+				if self.exit_button.clicked:
+					return 2
+			else:
+				self.level.run()
 
 			pygame.display.update()
 			self.clock.tick(clock_tick)
@@ -88,5 +72,4 @@ class Main:
 if __name__ == '__main__':
 	main = Main()
 	while main.run() != 2:
-		main = Main()
 		main.run()
